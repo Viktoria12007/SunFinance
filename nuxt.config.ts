833921aa3edfb7cb8cc8974237e14745bf15dcde
Виктория Голубова <v.golubova@denexy.com>
2025-10-24 22:05:29 +0600

    Added meta tags, '~/plugins/fontawesome', '@fortawesome/fontawesome-svg-core/styles.css' in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
+  modules: [
+      '@nuxtjs/tailwindcss'
+  ],
+  plugins: [
+      '~/plugins/fontawesome'
+  ],
+  app: {
+    head: {
+        title: 'SunFinance',
+        htmlAttrs: { lang: 'en' },
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        meta: [
+            { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        ],
+    },
+  },
 })
