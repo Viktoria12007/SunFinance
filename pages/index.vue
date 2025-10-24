6ca25150e87e5a1cc42f83b27fd1d52e8091ac43
Виктория Голубова <v.golubova@denexy.com>
2025-10-24 22:07:02 +0600

    Created default page.

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SearchCityInput from "~/components/UI/SearchCityInput.vue";
+</script>
+
+<template>
+  <div class="min-h-screen p-[25px]">
+    <SearchCityInput/>
+  </div>
+</template>
