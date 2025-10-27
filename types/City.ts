43249dfe0206721938b03690bbb5d7c6a1db05f2
Виктория Голубова <v.golubova@denexy.com>
2025-10-27 22:48:11 +0600

    Added types.

---
+++ types/City.ts
@@ -0,0 +1,46 @@
+export interface City {
+    clouds: Record<string, number> | null;
+    coord: { lat: number; lon: number };
+    dt: number;
+    id: number;
+    main: Record<string, number>;
+    name: string;
+    rain: Record<string, number> | null;
+    snow: Record<string, number> | null;
+    sys: Record<string, string>;
+    weather: Array<Record<string, string | number>>;
+    wind: Record<string, number>;
+}
+
+export interface CityList {
+    cod: string;
+    count: number;
+    list: Array<City>;
+    message: string;
+}
+
+export interface CityForecastDetails {
+    current: {
+        clouds: number;
+        dew_point: number;
+        dt: number;
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        sunrise: number;
+        sunset: number;
+        temp: number;
+        uvi: number;
+        visibility: number;
+        weather: Array<Record<string, string | number>>;
+        win_deg: number;
+        win_speed: number;
+    };
+    daily: Array<Record<string, string | number>>;
+    hourly: Array<Record<string, string | number>>;
+    lat: number;
+    lon: number;
+    minutely: Array<{ dt: number, precipitation: number }>;
+    timezone: string;
+    timezone_offset: number;
+}
