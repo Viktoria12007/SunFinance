a2458308b53dd3decbfde8a48188cd5ecd38f3c0
Виктория Голубова <v.golubova@denexy.com>
2025-10-30 19:33:08 +0600

    The global loader has been configured.

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import Loader from "~/components/UI/Loader.vue";
+import { useLoadingStore } from "~/stores/Loading";
+
+const store = useLoadingStore();
+const { isLoading } = storeToRefs(store);
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <Loader :isOpen="isLoading"/>
   </NuxtLayout>
 </template>
