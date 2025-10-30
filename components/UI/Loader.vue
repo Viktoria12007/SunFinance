639b8b34c2d7f1c504624789f54be5a94fb92aa1
Виктория Голубова <v.golubova@denexy.com>
2025-10-30 19:31:05 +0600

    Created component Loader.vue

---
+++ components/UI/Loader.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+const props = defineProps<{
+  isOpen: boolean,
+}>()
+</script>
+
+<template>
+  <Teleport to="body">
+    <div
+      v-if="isOpen"
+      class="fixed inset-[0] z-[1000] flex justify-center items-center bg-linear-to-b bg-[#000000] bg-[#272424B2]"
+    >
+      <FontAwesomeIcon
+        icon="fa-solid fa-spinner"
+        size="2xl"
+        class="text-white animate-spin"
+      />
+    </div>
+  </Teleport>
+</template>
