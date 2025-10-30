5d02df61dcaf572e220a2aed1b97ddeed5d0424a
Виктория Голубова <v.golubova@denexy.com>
2025-10-30 19:27:46 +0600

    Added icon 'faSpinner'.

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,10 +1,10 @@
 import { config, library } from "@fortawesome/fontawesome-svg-core";
-import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faSpinner, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 config.autoAddCss = false;
 
-library.add(faMagnifyingGlass, faXmark);
+library.add(faMagnifyingGlass, faXmark, faSpinner);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
